a5a27db8ad0bfebefb382e275554363a009c0289
Kam <kamyshova.ea@yandex.com>
2020-12-10 20:20:27 +0300

    I did todo list

---
+++ todolist/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <TodoList v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      todos: ['Встать с кровати', 'Завтрак', 'Школа', 'Обед', 'Уборка', 'Домашняя работа', 'ШП', 'Ужин', 'Фильм', 'Сон']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+        width: 700px;
+        margin: auto;
+        font-family: 'Arial', sans-serif;
+        font-size: 18px;
+        color: #35495e;
+    }
+</style>
---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul>
+        <li v-for="(todo, index) in todos" v-on:click="deleteTodo(index)" v-bind:key="index">{{ todo }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['todos'],
+    methods: {
+        deleteTodo(index){
+            this.$emit('delete-todo', index);
+        }
+    }
+}
+</script>
+<style>
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    li {
+        display: block;
+        border: 2px solid #008cf0;
+        margin: 10px 0;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+
+    li:hover {
+        background: #e0ffff;
+    }
+</style>
